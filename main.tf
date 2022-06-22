f5f74417f4f37917d9348ecb276a56d4affb42cb
Krishna Kanth Telaprolu <36309708+kktelaprolu@users.noreply.github.com>
2022-06-23 00:49:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,13 +7,3 @@ provider "google" {
 resource "google_pubsub_topic" "topic1" {
   name = "cloudfunction_test_topic"
 }
-
-resource "google_pubsub_topic" "topic2" {
-  name = "cloudfunction_test_topic2"
-}
-resource "google_pubsub_topic" "topic3" {
-  name = "cloudfunction_test_topic3"
-}
-resource "google_pubsub_topic" "topic4" {
-  name = "cloudfunction_test_topic4"
-}
